345e25a7e15e94850abfbb152abfa240ff2a73f2
VulkanoTG <pedrohh114@gmail.com>
2022-05-05 01:11:47 -0300

    function createTableTickets created
    need reviewed

--- src/main/java/thornyabot/thornyabot/Database/SQLite.java
+++ src/main/java/thornyabot/thornyabot/Database/SQLite.java
@@ -1,4 +1,27 @@
 package thornyabot.thornyabot.Database;
 
+import thornyabot.thornyabot.ThornyaBot;
+
 public class SQLite {
+
+    private static String url = null;
+
+    public SQLite(){
+        url = "jdbc:sqlite:" + ThornyaBot.pl.getDataFolder() + "/tickets.db";
+        createTableTickets();
+    }
+
+    private void createTableTickets(){
+        String sql = "Create TABLE IF NOT EXISTS Tickets (\n" +
+                "       id integer PRIMARY KEY AUTOINCREMENT,\n" +
+                "       nickname string NOT NULL,\n" +
+                "       type string NOT NULL,\n" +
+                "       message string NOT NULL,\n" +
+                "       player string,\n" +
+                "       staff string,\n" +
+                "       answer string,\n" +
+                "       isanswered string\n" +
+                ");";
+    }
+
 }

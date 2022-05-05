31f1bf00514b30edd3811b8fe9e619787170bc09
Gusttavo <gustavosousa918@gmail.com>
2022-05-04 23:31:53 -0300

    init project

---
+++ src/main/java/thornyabot/thornyabot/ThornyaBot.java
@@ -0,0 +1,17 @@
+package thornyabot.thornyabot;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ThornyaBot extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}

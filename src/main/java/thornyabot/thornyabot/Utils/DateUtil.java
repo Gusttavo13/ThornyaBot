9b914c0acf74e20445bb99e8c5e2604a5d61909a
Gusttavo <gustavosousa918@gmail.com>
2022-05-11 21:08:22 -0300

    add utils

---
+++ src/main/java/thornyabot/thornyabot/Utils/DateUtil.java
@@ -0,0 +1,208 @@
+package thornyabot.thornyabot.Utils;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class DateUtil {
+
+    private static final Map<Pattern, Long> TIME_UNITS = new HashMap<>();
+
+    static {
+        TIME_UNITS.put(Pattern.compile("((\\d+) ?(milliseconds|millisecond|ms))"), TimeUnit.MILLISECOND);
+        TIME_UNITS.put(Pattern.compile("((\\d+) ?(seconds|second|sec|s))"), TimeUnit.SECOND);
+        TIME_UNITS.put(Pattern.compile("((\\d+) ?(minutes|minute|min|m))"), TimeUnit.MINUTE);
+        TIME_UNITS.put(Pattern.compile("((\\d+) ?(hours|hour|h))"), TimeUnit.HOUR);
+        TIME_UNITS.put(Pattern.compile("((\\d+) ?(days|day|d))"), TimeUnit.DAY);
+        TIME_UNITS.put(Pattern.compile("((\\d+) ?(weeks|week|w))"), TimeUnit.WEEK);
+        TIME_UNITS.put(Pattern.compile("((\\d+) ?(months|month|mo))"), TimeUnit.MONTH);
+        TIME_UNITS.put(Pattern.compile("((\\d+) ?(years|year|y))"), TimeUnit.YEAR);
+    }
+
+    public static final Pattern SPECIFIC_DATE = Pattern.compile("(\\d+)/(\\d+)/(\\d+)");
+    public static final Pattern SPECIFIC_TIME = Pattern.compile("(\\d+):(\\d+):(\\d+)(:(\\d+))?");
+    public static final Pattern SPECIFIC_DATE_TIME = Pattern.compile("((\\d+)/(\\d+)/(\\d+)) ((\\d+):(\\d+):(\\d+)(:(\\d+))?)");
+
+    public static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd/MM/yyyy");
+    public static final SimpleDateFormat FULL_FORMAT = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+    public static final SimpleDateFormat TIME_FORMAT = new SimpleDateFormat("HH:mm:ss");
+
+    /**
+     * Returns a Date object from a specific date or time.
+     * @param date the date or time as String
+     * @return the Date object or null if the date or time is invalid
+     */
+    public static Date fromDate(String date) {
+        Date result = fromSpecificDate(date);
+        if (result != null)
+            return result;
+
+        result = fromSpecificTime(date);
+        if (result != null)
+            return result;
+
+        result = fromSpecificDateTime(date);
+        return result;
+    }
+
+    private static Date fromSpecificDate(String date) {
+        Matcher matcher = SPECIFIC_DATE.matcher(date);
+        if (!matcher.matches())
+            return null;
+
+        int day = Integer.parseInt(matcher.group(1));
+        int month = Integer.parseInt(matcher.group(2));
+        int year = Integer.parseInt(matcher.group(3));
+
+        if (day < 1 || day > 31)
+            return null;
+        if (month < 1 || month > 12)
+            return null;
+        if (year < 1)
+            return null;
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.DAY_OF_MONTH, day);
+        calendar.set(Calendar.MONTH, month - 1);
+        calendar.set(Calendar.YEAR, year);
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+
+        return calendar.getTime();
+    }
+
+    private static Date fromSpecificTime(String time) {
+        Matcher matcher = SPECIFIC_TIME.matcher(time);
+        if (!matcher.matches())
+            return null;
+
+        int hour = Integer.parseInt(matcher.group(1));
+        int minute = Integer.parseInt(matcher.group(2));
+        int second = Integer.parseInt(matcher.group(3));
+        int millisecond;
+        try {
+            millisecond = Integer.parseInt(matcher.group(5));
+        } catch (IndexOutOfBoundsException ignored) {
+            millisecond = 0;
+        }
+
+        if (hour < 0 || hour > 23)
+            return null;
+        if (minute < 0 || minute > 59)
+            return null;
+        if (second < 0 || second > 59)
+            return null;
+        if (millisecond < 0 || millisecond > 999)
+            return null;
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, hour);
+        calendar.set(Calendar.MINUTE, minute);
+        calendar.set(Calendar.SECOND, second);
+        calendar.set(Calendar.MILLISECOND, millisecond);
+
+        return calendar.getTime();
+    }
+
+    private static Date fromSpecificDateTime(String dateTime) {
+        Matcher matcher = SPECIFIC_DATE_TIME.matcher(dateTime);
+        if (!matcher.matches())
+            return null;
+
+        Date date = fromSpecificDate(matcher.group(1));
+        Date time = fromSpecificTime(matcher.group(5));
+
+        if (date == null || time == null)
+            return null;
+
+        return new Date(date.getTime() + time.getTime());
+    }
+
+    /**
+     * @param date1 the first date
+     * @param date2 the second date
+     * @param fullName if the full name of the time unit should be used
+     * @return the difference between the two dates in a beautiful String
+     */
+    public static String getDifference(long date1, long date2, boolean fullName) {
+        long diff = Math.abs(date1 - date2);
+        long diffSeconds = diff / 1000 % 60;
+        long diffMinutes = diff / (60 * 1000) % 60;
+        long diffHours = diff / (60 * 60 * 1000) % 24;
+        long diffDays = diff / (24 * 60 * 60 * 1000);
+
+        StringBuilder sb = new StringBuilder();
+        if (diffDays > 0)
+            sb.append(diffDays).append(fullName ? " days" : "d").append(" ");
+        if (diffHours > 0)
+            sb.append(diffHours).append(fullName ? " hours" : "h").append(" ");
+        if (diffMinutes > 0)
+            sb.append(diffMinutes).append(fullName ? " minutes" : "m").append(" ");
+        if (diffSeconds > 0)
+            sb.append(diffSeconds).append(fullName ? " seconds" : "s");
+        if (sb.length() == 0)
+            return fullName ? diff + " milliseconds" : diff + "ms";
+
+        return sb.toString().trim();
+    }
+
+    /**
+     * @param date the date
+     * @param fullName if the full name of the time unit should be used
+     * @return the difference between the current date and the given date in a beautiful String
+     */
+    public static String getDifference(long date, boolean fullName) {
+        return getDifference(date, System.currentTimeMillis(), fullName);
+    }
+
+    /**
+     * @param date the date
+     * @param fullName if the full name of the time unit should be used
+     * @return the date as a duration string
+     */
+    public static String toDuration(long date, boolean fullName) {
+        return getDifference(date, 0, fullName);
+    }
+
+    public static String format(long date, FormatType type) {
+        switch (type) {
+            case DATE:
+                return DATE_FORMAT.format(date);
+            case TIME:
+                return TIME_FORMAT.format(date);
+            case FULL:
+                return FULL_FORMAT.format(date);
+            default:
+                return null;
+        }
+    }
+
+    public static Long toMillis(String string) {
+        try {
+            return Long.parseLong(string) * 1000L;
+        } catch (NumberFormatException ignored) { //we need to convert it
+        }
+
+        long result = 0;
+
+        for (Map.Entry<Pattern, Long> entry : TIME_UNITS.entrySet()) {
+            final Matcher matcher = entry.getKey().matcher(string);
+            if (!matcher.find())
+                continue;
+
+            result += Long.parseLong(matcher.group(2)) * entry.getValue();
+        }
+
+        return result == 0 ? null : result;
+    }
+
+    public enum FormatType {
+        DATE, FULL, TIME
+    }
+}
---
+++ src/main/java/thornyabot/thornyabot/Utils/NumberFormatter.java
@@ -0,0 +1,62 @@
+package thornyabot.thornyabot.Utils;
+
+import java.text.DecimalFormat;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class NumberFormatter {
+
+    private static final DecimalFormat DECIMAL_FORMAT = new DecimalFormat("#.##");
+    private static final Pattern PATTERN = Pattern.compile("^(\\d+\\.?\\d*)(\\D+)");
+
+    public static final NumberFormatter SIMPLE_FORMATTER = new NumberFormatter();
+
+    private final List<String> suffixes;
+
+    public NumberFormatter() {
+        suffixes = Arrays.asList("", "K", "M", "B", "T", "Q", "QQ");
+    }
+
+    public NumberFormatter(List<String> suffixes) {
+        this.suffixes = suffixes;
+    }
+
+    public String formatNumber(double value) {
+        if (suffixes.isEmpty())
+            return DECIMAL_FORMAT.format(value);
+
+        boolean negative = value < 0;
+        int index = 0;
+        value = Math.abs(value);
+
+        double tmp;
+        while ((tmp = value / 1000) >= 1) {
+            if (index + 1 == suffixes.size())
+                break;
+            value = tmp;
+            ++index;
+        }
+
+        return (negative ? "-" : "") +  DECIMAL_FORMAT.format(value) + suffixes.get(index);
+    }
+
+    public double parseString(String value) throws NumberFormatException {
+        try {
+            return Double.parseDouble(value);
+        } catch (NumberFormatException ignored) {
+        }
+
+        Matcher matcher = PATTERN.matcher(value);
+        if (!matcher.find())
+            throw new NumberFormatException("Invalid format");
+
+        double amount = Double.parseDouble(matcher.group(1));
+        String suffix = matcher.group(2);
+
+        int index = suffixes.indexOf(suffix.toUpperCase());
+
+        return amount * Math.pow(1000, index);
+    }
+}
